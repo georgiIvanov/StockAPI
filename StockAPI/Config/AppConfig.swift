25c00eeafe65ce2487a761043074448e8e154864
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2020-09-20 00:45:30 +0300

    Added config files for Debug/Release

--- StockAPI/Config/AppConfig.swift
+++ StockAPI/Config/AppConfig.swift
@@ -9,12 +9,31 @@ import Foundation
 
 struct AppConfig {
     
-    static var apiBaseUrl: String {
-        return "www.alphavantage.co"
+    static var stockApiBaseUrl: String {
+        return configValue(for: "STOCK_API_BASE_URL")
+    }
+    
+    static var stockApiKey: String {
+        return configValue(for: "STOCK_API_KEY")
     }
     
     static var stubResponses: Bool {
-        return false
+        return configValue(for: "STUB_RESPONSE")
     }
     
+    static func configValue<T>(for key: String) -> T where T: LosslessStringConvertible {
+        guard let object = Bundle.main.object(forInfoDictionaryKey: key) else {
+            fatalError("Missing Info.plist key: \(key)")
+        }
+
+        switch object {
+        case let value as T:
+            return value
+        case let string as String:
+            guard let value = T(string) else { fallthrough }
+            return value
+        default:
+            fatalError("Invalid value with key: \(key)")
+        }
+    }
 }
