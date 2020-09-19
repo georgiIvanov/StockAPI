e4e849307639b56914819ea4ca1c63152e89dff5
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2020-09-19 22:52:04 +0300

    Stock symbols enum and StockQuote model

---
+++ StockAPI/Models/StockQuote.swift
@@ -0,0 +1,16 @@
+//
+//  StockQuote.swift
+//  StockAPI
+//
+//  Created by Voro on 19.09.20.
+//
+
+import Foundation
+
+struct StockQuote {
+    let open: Double
+    let high: Double
+    let low: Double
+    let close: Double
+    let volume: Double
+}
---
+++ StockAPI/Models/StockSymbol.swift
@@ -0,0 +1,16 @@
+//
+//  StockSymbol.swift
+//  StockAPI
+//
+//  Created by Voro on 19.09.20.
+//
+
+import Foundation
+
+enum StockSymbol: String, CaseIterable {
+    case ibm = "IBM"
+    case microsoft = "MSFT"
+    case apple = "AAPL"
+    case googleA = "GOOGL"
+    case facebook = "FB"
+}
